597a52ee9da9671c9fca715a28a90cc97bbdff71
François Garillot <francois@skymind.io>
2017-05-01 18:45:58 -0400

    More coplete aggregation structure

--- datavec-api/src/main/java/org/datavec/api/transform/ops/AggregatableReduceOp.java
+++ datavec-api/src/main/java/org/datavec/api/transform/ops/AggregatableReduceOp.java
@@ -3,14 +3,14 @@ package org.datavec.api.transform.ops;
 /**
  * Created by huitseeker on 4/28/17.
  */
-public interface AggregatebleReduceOp<T, U, V> {
+public interface AggregatableReduceOp<T, U, V> {
 
-    public U add(U accumulator, T element);
+    public U tally(U accumulator, T element);
 
     public U combine(U accu1, U accu2);
 
     public U neutral();
 
-    public V finalAggregate(U acc);
+    public V summarize(U acc);
 
 }
---
+++ datavec-api/src/main/java/org/datavec/api/transform/ops/AggregatorImpls.java
@@ -0,0 +1,292 @@
+package org.datavec.api.transform.ops;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+
+/** Created by huitseeker on 4/28/17. */
+public class AggregatorImpls {
+
+  public static class AggregatableFirst<T> extends AggregatorOp<T, T, T> {
+
+    @Override
+    public T tally(T accumulator, T element) {
+      return accumulator == null ? element : accumulator;
+    }
+
+    @Override
+    public T combine(T accu1, T accu2) {
+      return accu1;
+    }
+
+    @Override
+    public T neutral() {
+      return null;
+    }
+
+    @Override
+    public T summarize(T acc) {
+      return acc;
+    }
+  }
+
+  public static class AggregatableLast<T> extends AggregatorOp<T, T, T> {
+
+    @Override
+    public T tally(T accumulator, T element) {
+      return element;
+    }
+
+    @Override
+    public T combine(T accu1, T accu2) {
+      return accu2;
+    }
+
+    @Override
+    public T neutral() {
+      return null;
+    }
+
+    @Override
+    public T summarize(T acc) {
+      return acc;
+    }
+  }
+
+  public static class AggregatableSum extends AggregatorOp<Number, Double, Double> {
+
+    public Double tally(Double accumulator, Number n) {
+      return accumulator + n.doubleValue();
+    }
+
+    public Double combine(Double acc1, Double acc2) {
+      return acc1 + acc2;
+    }
+
+    public Double neutral() {
+      return 0D;
+    }
+
+    public Double summarize(Double acc) {
+      return acc;
+    }
+  }
+
+  public static class AggregatableProd extends AggregatorOp<Number, Double, Double> {
+
+    public Double tally(Double accumulator, Number n) {
+      return accumulator * n.doubleValue();
+    }
+
+    public Double combine(Double acc1, Double acc2) {
+      return acc1 * acc2;
+    }
+
+    public Double neutral() {
+      return 1D;
+    }
+
+    public Double summarize(Double acc) {
+      return acc;
+    }
+  }
+
+  public static class AggregatableCount<T> extends AggregatorOp<T, Long, Long> {
+
+    public Long tally(Long accumulator, T n) {
+      return accumulator + 1;
+    }
+
+    public Long combine(Long acc1, Long acc2) {
+      return acc1 + acc2;
+    }
+
+    public Long neutral() {
+      return 0L;
+    }
+
+    public Long summarize(Long acc) {
+      return acc;
+    }
+  }
+
+  public static class AggregatableMean extends AggregatorOp<Number, Pair<Long, Double>, Double> {
+
+    public Pair<Long, Double> tally(Pair<Long, Double> accumulator, Number n) {
+
+      Long count = accumulator.getLeft();
+      Double mean = accumulator.getRight();
+
+      // See Knuth TAOCP vol 2, 3rd edition, page 232
+      if (count == 0) {
+        return Pair.of(1L, n.doubleValue());
+      } else {
+        Long newCount = count + 1;
+        Double newMean = mean + (n.doubleValue() - mean) / newCount;
+        return Pair.of(newCount, newMean);
+      }
+    }
+
+    public Pair<Long, Double> combine(Pair<Long, Double> acc1, Pair<Long, Double> acc2) {
+      Long totalCount = acc1.getLeft() + acc2.getLeft();
+      Double totalMean =
+          (acc1.getRight() * acc1.getLeft() + acc2.getRight() * acc1.getLeft()) / totalCount;
+      return Pair.of(totalCount, totalMean);
+    }
+
+    public Pair<Long, Double> neutral() {
+      return Pair.of(0L, 0D);
+    }
+
+    public Double summarize(Pair<Long, Double> acc) {
+      return acc.getRight();
+    }
+  }
+
+  public static class AggregatableStdDev
+      extends AggregatorOp<Number, Triple<Long, Double, Double>, Double> {
+
+    public Triple<Long, Double, Double> tally(Triple<Long, Double, Double> accumulator, Number n) {
+
+      Long count = accumulator.getLeft();
+      Double mean = accumulator.getMiddle();
+      Double s = accumulator.getRight();
+
+      // See Knuth TAOCP vol 2, 3rd edition, page 232
+      if (count == 0) {
+        return Triple.of(1L, n.doubleValue(), 0D);
+      } else {
+        Long newCount = count + 1;
+        Double newMean = mean + (n.doubleValue() - mean) / newCount;
+        Double newS = s + (n.doubleValue() - mean) * (n.doubleValue() - newMean);
+        return Triple.of(newCount, newMean, newS);
+      }
+    }
+
+    public Triple<Long, Double, Double> combine(
+        Triple<Long, Double, Double> acc1, Triple<Long, Double, Double> acc2) {
+      Long totalCount = acc1.getLeft() + acc2.getLeft();
+      Double totalMean =
+          (acc1.getMiddle() * acc1.getLeft() + acc2.getMiddle() * acc1.getLeft()) / totalCount;
+      // the variance of the union is the sum of variances
+      Double leftVariance = acc1.getRight() / (acc1.getLeft() - 1);
+      Double rightvariance = acc2.getRight() / (acc2.getLeft() - 1);
+      Double totalS = (leftVariance + rightvariance) * (totalCount - 1);
+      return Triple.of(totalCount, totalMean, totalS);
+    }
+
+    public Triple<Long, Double, Double> neutral() {
+      return Triple.of(0L, 0D, 0D);
+    }
+
+    public Double summarize(Triple<Long, Double, Double> acc) {
+      return Math.sqrt(acc.getRight() / (acc.getLeft() - 1));
+    }
+  }
+
+  public class AggregatableFunction<T> extends AggregatorOp<T, T, T> {
+
+    private DecoratedBiFunction<T, T, T> function;
+    private T neutralElement;
+
+    public AggregatableFunction(DecoratedBiFunction<T, T, T> fun, T neut) {
+      function = fun;
+      neutralElement = neut;
+    }
+
+    public final T tally(T accumulator, T number) {
+      return function.apply(accumulator, number);
+    }
+
+    public final T combine(T acc1, T acc2) {
+      return function.apply(acc1, acc2);
+    }
+
+    public final T neutral() {
+      return neutralElement;
+    }
+
+    public final T summarize(T accu) {
+      return accu;
+    }
+  }
+
+  public final AggregatableReduceOp<Long, Long, Long> minLong =
+      new AggregatableFunction<Long>(
+          new DecoratedBiFunction<Long, Long, Long>() {
+            @Override
+            public Long apply(Long x, Long y) {
+              return Math.min(x, y);
+            }
+          },
+          Long.MAX_VALUE);
+
+  public final AggregatableReduceOp<Integer, Integer, Integer> minInt =
+      new AggregatableFunction<Integer>(
+          new DecoratedBiFunction<Integer, Integer, Integer>() {
+            @Override
+            public Integer apply(Integer x, Integer y) {
+              return Math.min(x, y);
+            }
+          },
+          Integer.MAX_VALUE);
+
+  public final AggregatableReduceOp<Double, Double, Double> minDouble =
+      new AggregatableFunction<Double>(
+          new DecoratedBiFunction<Double, Double, Double>() {
+            @Override
+            public Double apply(Double x, Double y) {
+              return Math.min(x, y);
+            }
+          },
+          Double.MAX_VALUE);
+
+  public final AggregatableReduceOp<Float, Float, Float> minFloat =
+      new AggregatableFunction<Float>(
+          new DecoratedBiFunction<Float, Float, Float>() {
+            @Override
+            public Float apply(Float x, Float y) {
+              return Math.min(x, y);
+            }
+          },
+          Float.MAX_VALUE);
+
+  public final AggregatableReduceOp<Long, Long, Long> maxLong =
+      new AggregatableFunction<Long>(
+          new DecoratedBiFunction<Long, Long, Long>() {
+            @Override
+            public Long apply(Long x, Long y) {
+              return Math.max(x, y);
+            }
+          },
+          Long.MIN_VALUE);
+
+  public final AggregatableReduceOp<Integer, Integer, Integer> maxInt =
+      new AggregatableFunction<Integer>(
+          new DecoratedBiFunction<Integer, Integer, Integer>() {
+            @Override
+            public Integer apply(Integer x, Integer y) {
+              return Math.min(x, y);
+            }
+          },
+          Integer.MIN_VALUE);
+
+  public final AggregatableReduceOp<Double, Double, Double> maxDouble =
+      new AggregatableFunction<Double>(
+          new DecoratedBiFunction<Double, Double, Double>() {
+            @Override
+            public Double apply(Double x, Double y) {
+              return Math.min(x, y);
+            }
+          },
+          Double.MIN_VALUE);
+
+  public final AggregatableReduceOp<Float, Float, Float> maxFloat =
+      new AggregatableFunction<Float>(
+          new DecoratedBiFunction<Float, Float, Float>() {
+            @Override
+            public Float apply(Float x, Float y) {
+              return Math.min(x, y);
+            }
+          },
+          Float.MIN_VALUE);
+}

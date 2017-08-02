a34572c26c7e4daa6e754ea460d8509a320e81d7
François Garillot <francois@garillot.net>
2017-08-02 17:13:00 -0400

    special case spark-dependent versions

--- contrib/release-tag.sh
+++ contrib/release-tag.sh
@@ -25,15 +25,17 @@ MVN="mvn"
 # Create release version
 $MVN versions:set -DnewVersion=$RELEASE_VERSION | grep -v "no value" # silence logs
 
+# Set version in datavec-spark-inference-parent/datavec-spark-inference-client/pom.xml
+sed -i".tmp1" -Ee 's/<version>([0-9.]+)_(spark_[0-9])(-SNAPSHOT)?/<version>'"$RELEASE_VERSION"'_\2/g' datavec-spark-inference-parent/datavec-spark-inference-client/pom.xml
+sed -i".tmp1" -Ee 's/<version>([0-9.]+)_(spark_[0-9])(-SNAPSHOT)?/<version>'"$RELEASE_VERSION"'_\2/g' datavec-spark-inference-parent/datavec-spark-inference-server/pom.xml
+
 git commit -a -m "Preparing release $RELEASE_TAG"
 echo "Creating tag $RELEASE_TAG at the head of $GIT_BRANCH"
 git tag $RELEASE_TAG
 
 # Create next version
 $MVN versions:set -DnewVersion=$NEXT_VERSION | grep -v "no value" # silence logs
+sed -i".tmp1" -Ee 's/<version>'"$RELEASE_VERSION"'_(spark_[0-9])/<version>'"${NEXT_VERSION%-SNAPSHOT}"'_\1-SNAPSHOT/g' datavec-spark-inference-parent/datavec-spark-inference-client/pom.xml
+sed -i".tmp1" -Ee 's/<version>'"$RELEASE_VERSION"'_(spark_[0-9])/<version>'"${NEXT_VERSION%-SNAPSHOT}"'_\1-SNAPSHOT/g' datavec-spark-inference-parent/datavec-spark-inference-server/pom.xml
 
 git commit -a -m "Preparing development version $NEXT_VERSION"
-
-# Push changes
-# git push origin $RELEASE_TAG
-# git push origin HEAD:$GIT_BRANCH
